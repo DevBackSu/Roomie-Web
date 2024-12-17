646ecdcb997549b4d193d9dfbbb6a8152ca45da0
unknown <ansumin0103@gmail.com>
2024-12-17 09:10:40 +0900

    info page

--- src/pages/InfoPage.js
+++ src/pages/InfoPage.js
@@ -67,6 +67,7 @@ function InfoPage() {
         <div>
             <h1>정보 입력</h1>
             <form onSubmit={handleSubmit}>
+                {/* 이름 */}
                 <div>
                     <label htmlFor="nickname">이름:</label>
                     <input
@@ -78,6 +79,8 @@ function InfoPage() {
                         required
                     />
                 </div>
+                
+                {/* 이메일 */}
                 <div>
                     <label htmlFor="email">이메일:</label>
                     <input
@@ -89,6 +92,115 @@ function InfoPage() {
                         required
                     />
                 </div>
+
+                {/* 성별 */}
+                <div>
+                    <label>성별:</label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="남"
+                            checked={formData.gender === "남"}
+                            onChange={handleChange}
+                            required
+                        />
+                        남
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="여"
+                            checked={formData.gender === "여"}
+                            onChange={handleChange}
+                            required
+                        />
+                        여
+                    </label>
+                </div>
+
+                {/* 주요 동물 */}
+                <div>
+                    <label>주요 동물:</label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="mainAnimal"
+                            value="부엉이"
+                            checked={formData.mainAnimal === "부엉이"}
+                            onChange={handleChange}
+                            required
+                        />
+                        부엉이
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="mainAnimal"
+                            value="종달새"
+                            checked={formData.mainAnimal === "종달새"}
+                            onChange={handleChange}
+                            required
+                        />
+                        종달새
+                    </label>
+                </div>
+
+                {/* 생년월일 */}
+                <div>
+                    <label htmlFor="birthDate">생년월일:</label>
+                    <input
+                        type="month"
+                        id="birthDate"
+                        name="birthDate"
+                        value={formData.birthDate}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                {/* 학교 */}
+                <div>
+                    <label htmlFor="school">학교 (선택):</label>
+                    <input
+                        type="text"
+                        id="school"
+                        name="school"
+                        value={formData.school}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                {/* 지역 */}
+                <div>
+                    <label htmlFor="local">지역 (선택):</label>
+                    <input
+                        type="text"
+                        id="local"
+                        name="local"
+                        value={formData.local}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                {/* 프로필 이미지 */}
+                <div>
+                    <label>프로필 이미지:</label>
+                    {[1, 2, 3, 4, 5].map((num) => (
+                        <label key={num}>
+                            <input
+                                type="radio"
+                                name="imgUrl"
+                                value={num}
+                                checked={formData.imgUrl === String(num)}
+                                onChange={handleChange}
+                            />
+                            {num}
+                        </label>
+                    ))}
+                </div>
+
                 <button type="submit">저장</button>
             </form>
         </div>
